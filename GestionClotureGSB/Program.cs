49b6a99a68cc0b5048441fcaafd9bb99baf2b31c
alice.bord1 <alice.bord1@gmail.com>
2019-03-08 19:46:29 +0100

    Ajoutez des fichiers projet.

---
+++ GestionClotureGSB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestionClotureGSB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
