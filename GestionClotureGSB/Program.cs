e47fc7a47f6184bafd9095dc3f86c1dcc60b2be9
alice.bord1 <alice.bord1@gmail.com>
2019-03-09 14:00:41 +0100

    Création d'un fichier xml "docGsb.xml" contenant l'ensemble des balises de la documentation technique. Des balises <include /> ont remplacé l'amat d'annotation dans le code source pour plus de visibilité.

--- GestionClotureGSB/Program.cs
+++ GestionClotureGSB/Program.cs
@@ -5,39 +5,32 @@ using MyTools;
 
 namespace GestionClotureGSB
 {
-    /// <summary>
-    /// Classe Program. 
-    /// Contient l'ensemble des méthodes permettant d'assurer la gestion de la campagne de validation des fiches de frais par les comptables de la société GSB.
-    /// L'ensemble de l'application est déclenché automatiquement par un timer.
-    /// Permet la clôture des fiches du mois N-1 entre le 1er et le 10 du mois.
-    /// Permet de modifier une fiche de frais remboursée à partir du 20ème jour du mois (modification de son état de "MP" à "RB").
-    /// </summary>
+    /*
+     * Contient toutes les méthodes pour clôturer et mettre à remboursées les fiches de frais.
+     */
+    /// <include file='docGsb.xml' path='doc/members[@name="gsb"]/Program/*'/>
     class Program
     {
         // Propriétés.
-        /// <value>Prend la valeur de la date du jour.</value>
+        /// <include file='docGsb.xml' path='doc/members[@name="gsb"]/actualDate/*'/>
         private static DateTime actualDate = DateTime.Today;
-        /// <value>Contiendra une instance de la classe BDConnection.</value>
+        /// <include file='docGsb.xml' path='doc/members[@name="gsb"]/maCnx/*'/>
         private static BDConnection maCnx;
+        /// <include file = 'docGsb.xml' path='doc/members[@name="gsb"]/myTimer/*'/>
         private static System.Timers.Timer myTimer;
 
-        /// <summary>
-        /// Valorise la propriété privée et statique maCnx avec une nouvelle instance de la classe BDConnection.
-        /// </summary>
+        // Connexion à la base de données.
+        /// <include file = 'docGsb.xml' path='doc/members[@name="gsb"]/myTimer/*'/>
         private static void GetConnection()
         {
             Program.maCnx = MyTools.BDConnection.GetBDConnection("localhost", "gsb_frais", "root", "root");
         }
 
-        /// <summary>
-        /// Paramétrage du timer en fonction de l'intervalle de déclenchement souhaité. 
-        /// Si 0 envoyé en paramètre : 
-        /// Comportement par défaut du timer : déclenchement toutes les 2 mins.
-        /// </summary>
-        /// <param name="interval">Intervalle auquel doit se déclencher le timer.</param>
+        // Paramétrage du timer en fonction de l'intervalle de déclenchement souhaité. 
+        /// <include file='docGsb.xml' path='doc/members[@name="gsb"]/ParametrageTimer/*'/>
         private static void ParametrageTimer(double interval)
         {
-            // Paramétrage du timer
+            // Paramétrage du timer.
             if (interval != 0)
             {
                 // Contient une valeur fixée par l'appelant de l'application dans le main.
@@ -51,19 +44,15 @@ namespace GestionClotureGSB
             }
         }
 
-        /// <summary>
-        ///  Méthode appelée dans le Main.
-        /// S'occupe du lancement de l'application.
-        /// Fonctionnalités de debug avec affichages consoles et possibilité de stopper à tout moment le processus.
-        /// </summary>
-        /// <param name="interval">Interval auquel doit se déclencher le timer.</param>
+        // Méthode appelée dans le Main : S'occupe du lancement de l'application.
+        /// <include file='docGsb.xml' path='doc/members[@name="gsb"]/Application/*'/>
         public static void Application(double interval)
         {
             // Paramétrage du timer.
             ParametrageTimer(interval);
 
             Console.WriteLine("Appuyez sur la touche 'Entrée' pour quitter l'application");
-            Console.WriteLine("L'application a commencé le" + DateTime.Now);
+            Console.WriteLine("L'application a commencé le " + DateTime.Now);
             Console.ReadLine();
             Console.WriteLine("Terminaison de l'application...");
             // Exécuté dès lors qu'on appuie sur une touche du clavier. 
@@ -74,12 +63,8 @@ namespace GestionClotureGSB
             Console.WriteLine("Fin.");
         }
 
-        /// <summary>
-        /// Paramètrage du timer. 
-        /// Instanciation d'un timer avec l'intervalle envoyé en paramètre.
-        /// Ajout d'un événement déclencheur : déclenchement du timer.
-        /// </summary>
-        /// <param name="interval">Intervalle auquel doit se déclencher le timer.</param>
+        // Paramètrage du timer. 
+        /// <include file='docGsb.xml' path='doc/members[@name="gsb"]/SetTimer/*'/>
         private static void SetTimer(double interval)
         {
             // Création d'un timer avec envoi d'un intervalle en paramètre.
@@ -92,12 +77,9 @@ namespace GestionClotureGSB
             myTimer.Enabled = true;
         }
 
-        /// <summary>
-        /// Evénement qui se déclenche lorsque le timer est lui-même déclenché.
+        // Evénement qui se déclenche lorsque le timer est lui-même déclenché.
         /// Contient les méthodes métier à exécuter.
-        /// </summary>
-        /// <param name="source">Source du déclenchement de l'événement.</param>
-        /// <param name="e">Fournit des données pour l'événement Elapsed (déclenché).</param>
+        /// <include file='docGsb.xml' path='doc/members[@name="gsb"]/AuDeclenchementTimer/*'/>
         private static void AuDeclenchementTimer(Object source, ElapsedEventArgs e)
         {
             Console.WriteLine("\nl'événement 'chronométrage' a été déclenché le "+ e.SignalTime +"\n");
@@ -105,11 +87,8 @@ namespace GestionClotureGSB
             RembourserFicheFrais();
         }
 
-        /// <summary>
-        /// Campagne de validation des fiches de frais,
-        /// Si nous nous trouvons entre le 1 et le 10 du mois courant, 
-        /// cloture des fiches du mois N-1 en passant leur état de "CR" à "CL".
-        /// </summary>
+        // Campagne de validation des fiches de frais,
+        /// <include file='docGsb.xml' path='doc/members[@name="gsb"]/CloturerFicheFrais/*'/>
         private static void CloturerFicheFrais()
         {
             // Si on se trouve actuellement entre le 1er et le 10 :
@@ -123,11 +102,8 @@ namespace GestionClotureGSB
             }
         }
 
-        /// <summary>
-        /// A partir du 20ème jour du mois,
-        /// mise à jour de l'état de la fiche de frais ("MP" à "RB"),
-        /// pour informer que celle-ci a bien été remboursée.
-        /// </summary>
+        // Information de remboursement de fiches de frais.
+        /// <include file="docGsb.xml" path="doc/members[name='gsb']/RembourserFicheFrais/*"/>
         private static void RembourserFicheFrais()
         {
             // A partir du 20ème jour du mois : 
